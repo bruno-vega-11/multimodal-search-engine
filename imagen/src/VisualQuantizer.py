1afb5e94e2f2a11cf37342cb419491d1399a5cda
DiazBv <nicolasdiaz140220@gmail.com>
2026-07-05 14:13:57 -0500

    agregando metricas y cambiando vectores imagen

--- imagen/src/VisualQuantizer.py
+++ imagen/src/VisualQuantizer.py
@@ -1,19 +1,15 @@
 import cv2
 import numpy as np
 import faiss
+from metrics import MetricsTracker 
 
 class VisualQuantizer:
     def __init__(self, codebook_npy_path, k_clusters=1000):
-        print("Inicializando motor de cuantización visual con FAISS...")
-        
-        self.centroids = np.load(codebook_npy_path).astype(np.float32)
+        self.centroids = np.load(codebook_npy_path, mmap_mode='r').astype(np.float32)
         self.k = k_clusters
-        
         self.index = faiss.IndexFlatL2(128) 
         self.index.add(self.centroids)
-        
         self.sift = cv2.SIFT_create()
-        print("Motor de inferencia listo.")
 
     def image_to_histogram(self, image_path):
         img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
@@ -21,7 +17,6 @@ class VisualQuantizer:
             return np.zeros(self.k).tolist()
 
         keypoints, descriptors = self.sift.detectAndCompute(img, None)
-        
         del img
         
         if descriptors is None:
@@ -31,25 +26,20 @@ class VisualQuantizer:
         _, indices = self.index.search(descriptors, 1)
         
         visual_words = indices.flatten()
-
         histogram, _ = np.histogram(visual_words, bins=np.arange(self.k + 1), density=False)
 
         return histogram.tolist()
 
 if __name__ == "__main__":
     quantizer = VisualQuantizer("./codebook_kmeans.npy", k_clusters=1000)
-    
     imagen_prueba = "test_image.jpg" 
     
-    try:
-        vector = quantizer.image_to_histogram(imagen_prueba)
-        print("\n¡Imagen cuantizada exitosamente!")
-        print(f"Longitud del vector: {len(vector)}")
-        print(f"Muestra (primeros 15 valores): {vector[:15]}")
-    except Exception as e:
-        print(f"Error al probar: {e}")
-        
-        
-
-
-
+    # Uso de MetricsTracker igual que en el motor de búsqueda de texto
+    with MetricsTracker() as m:
+        try:
+            vector = quantizer.image_to_histogram(imagen_prueba)
+            print("\n¡Imagen cuantizada exitosamente!")
+        except Exception as e:
+            print(f"Error al probar: {e}")
+            
+    print("Metricas de procesamiento:", m.result)
